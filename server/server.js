721883d7dfb54333f600ad528457f4940d79be6f
Geert Weening <geertweening@gmail.com>
2014-04-18 12:32:47 -0700

    [FEATURE] add static routing

--- server/server.js
+++ server/server.js
@@ -16,11 +16,14 @@ if (true || 'development' === process.env.NODE_ENV || 'test' === process.env.NOD
 // set up middleware
 app.disable('x-powered-by');
 
+
 app.use(function(req,res,next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
+
+app.use(express.static(__dirname + '/../client'));
 app.use(express.bodyParser());
 
 
