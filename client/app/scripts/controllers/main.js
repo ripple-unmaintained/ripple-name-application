3d2a58cae087702fca6085e38d16c706eec6268f
Abiy Seifu <abiy@ripple.com>
2014-04-21 00:33:16 -0700

    [FEATURE] added null if empty directive for $dirty inputs

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -1,27 +1,27 @@
 'use strict';
 
-rippleNames.controller('MainCtrl', function ($scope, $http) {
-//    $scope.rippleName = {
-//        full_name: "Abiy",
-//        organization: "Ripple",
-//        email: "abiy@ripple.com",
-//        phone: '9879879879',
-//        website: 'http://www.ripple.com',
-//        ripple_name: 'Ripple name',
-//        ripple_address: 'iuyweiruywqer7123',
-//        justification: 'Long sentence'
-//    };
+rippleNames.directive('nullIfEmpty', [function() {
+        return {
+            require: 'ngModel',
+            link: function(scope, elm, attr, ctrl) {
+                ctrl.$parsers.unshift(function(value) {
+                    return value === '' ? null : value;
+                });
+            }
+        };
+    }]
+);
 
+rippleNames.controller('MainCtrl', function ($scope, $http) {
+    //form status
     $scope.isSubmitting = false;
     $scope.submittedSuccessfully = false;
 
     $scope.submitForm = function(isValid){
 
-        //form statuses
-        $scope.submitted = true;
+        //form status
         $scope.submitErrors = [];
 
-        console.log(isValid);
         if(isValid){
             $scope.isSubmitting = true;
             $http.post('/v1/application', $scope.rippleName).success(successCallback).error(errorCallback);
@@ -29,7 +29,6 @@ rippleNames.controller('MainCtrl', function ($scope, $http) {
 
         function successCallback(data){
             $scope.submittedSuccessfully = true;
-            console.log(data);
         }
         
         function errorCallback(data){
